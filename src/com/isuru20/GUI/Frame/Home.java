ffa404e5a19a73ec0d413b4957cf034796af42ae
Isuru Priyamantha <155815653+isuru20ip@users.noreply.github.com>
2025-01-21 18:21:01 +0530

    create panel load method
    - set panel loader
    - set avoid the creating same panel multiple time.
    - add jframe background color to black

--- src/com/isuru20/GUI/Frame/Home.java
+++ src/com/isuru20/GUI/Frame/Home.java
@@ -1,13 +1,20 @@
 package com.isuru20.GUI.Frame;
 
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import com.isuru20.GUI.panal.Dashboard;
+import com.isuru20.GUI.panal.Student;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Toolkit;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Home extends javax.swing.JFrame {
 
     public Home() {
         initComponents();
         frameSetup();
+        panalSetup();
     }
 
     @SuppressWarnings("unchecked")
@@ -15,6 +22,7 @@ public class Home extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jButton2 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
@@ -24,76 +32,114 @@ public class Home extends javax.swing.JFrame {
         jButton9 = new javax.swing.JButton();
         jButton11 = new javax.swing.JButton();
         jButton12 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
+        main = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new java.awt.GridLayout(1, 12));
 
-        jButton1.setText("jButton1");
-        jPanel1.add(jButton1);
-
-        jButton3.setText("jButton3");
-        jPanel1.add(jButton3);
-
-        jButton4.setText("jButton4");
-        jPanel1.add(jButton4);
-
-        jButton6.setText("jButton6");
-        jPanel1.add(jButton6);
-
-        jButton7.setText("jButton7");
-        jPanel1.add(jButton7);
-
-        jButton8.setText("jButton8");
-        jPanel1.add(jButton8);
-
-        jButton9.setText("jButton9");
-        jPanel1.add(jButton9);
-
-        jButton11.setText("jButton11");
-        jPanel1.add(jButton11);
-
-        jButton12.setText("jButton12");
-        jPanel1.add(jButton12);
-
-        jButton10.setText("jButton10");
-        jPanel1.add(jButton10);
-
-        jButton5.setText("jButton5");
-        jPanel1.add(jButton5);
-
-        jButton2.setText("jButton2");
+        jButton2.setBackground(new java.awt.Color(102, 153, 255));
+        jButton2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(0, 0, 0));
+        jButton2.setText("Dashboard");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton2);
 
-        jPanel2.setBackground(new java.awt.Color(255, 0, 0));
-        jPanel2.setLayout(new java.awt.BorderLayout());
+        jButton1.setBackground(new java.awt.Color(255, 204, 102));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("Students");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1);
+
+        jButton3.setBackground(new java.awt.Color(255, 204, 102));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(0, 0, 0));
+        jButton3.setText("Teachers");
+        jPanel1.add(jButton3);
+
+        jButton4.setBackground(new java.awt.Color(255, 204, 102));
+        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(0, 0, 0));
+        jButton4.setText("Payments");
+        jPanel1.add(jButton4);
+
+        jButton6.setBackground(new java.awt.Color(255, 204, 102));
+        jButton6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton6.setForeground(new java.awt.Color(0, 0, 0));
+        jButton6.setText("Subjects");
+        jPanel1.add(jButton6);
+
+        jButton7.setBackground(new java.awt.Color(255, 204, 102));
+        jButton7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton7.setForeground(new java.awt.Color(0, 0, 0));
+        jButton7.setText("Shedule");
+        jPanel1.add(jButton7);
+
+        jButton8.setBackground(new java.awt.Color(255, 204, 102));
+        jButton8.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton8.setForeground(new java.awt.Color(0, 0, 0));
+        jButton8.setText("Badge");
+        jPanel1.add(jButton8);
+
+        jButton9.setBackground(new java.awt.Color(255, 204, 102));
+        jButton9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton9.setForeground(new java.awt.Color(0, 0, 0));
+        jButton9.setText("Attendance");
+        jPanel1.add(jButton9);
+
+        jButton11.setBackground(new java.awt.Color(255, 204, 102));
+        jButton11.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton11.setForeground(new java.awt.Color(0, 0, 0));
+        jButton11.setText("Admin");
+        jPanel1.add(jButton11);
+
+        jButton12.setBackground(new java.awt.Color(255, 153, 153));
+        jButton12.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton12.setForeground(new java.awt.Color(0, 0, 0));
+        jButton12.setText("Logout");
+        jPanel1.add(jButton12);
+
+        main.setLayout(new java.awt.BorderLayout());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1000, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(main, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(main, javax.swing.GroupLayout.DEFAULT_SIZE, 479, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        loadPanel(new Student());
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        loadPanel(new Dashboard());
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -109,25 +155,44 @@ public class Home extends javax.swing.JFrame {
 
     private void frameSetup() {
         this.setTitle("VT School System");
-        this.setSize(900, 600); // Initial size
+        //this.setSize(900, 600); // Initial size
+        this.setBackground(Color.BLACK);
         this.setExtendedState(MAXIMIZED_BOTH);
         this.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/com/isuru20/assets/top_icon.png")));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel main;
     // End of variables declaration//GEN-END:variables
+
+    private JPanel current;
+
+    private void loadPanel(JPanel panel) {
+
+        if (!current.getClass().equals(panel.getClass())) {
+            main.removeAll();
+            main.add(panel, BorderLayout.CENTER);
+            SwingUtilities.updateComponentTreeUI(main);
+            current = panel;
+        } else {
+            System.out.println("error");
+        }
+    }
+
+    private void panalSetup() {
+        current = new Dashboard();
+        main.add(current, BorderLayout.CENTER);
+    }
+
 }
