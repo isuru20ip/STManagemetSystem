d447033e57ef3b88f2be6cac039b847dbd41c4b8
Isuru Priyamantha <155815653+isuru20ip@users.noreply.github.com>
2025-01-28 15:37:16 +0530

    payment Panal

---
+++ src/com/isuru20/GUI/panal/Payment.java
@@ -0,0 +1,30 @@
+package com.isuru20.GUI.panal;
+
+public class Payment extends javax.swing.JPanel {
+
+    public Payment() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setPreferredSize(new java.awt.Dimension(998, 537));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 998, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 584, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+}
